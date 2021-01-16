12735ae15ad4b44db26d8dd96f29e553578e6b87
Vladox69 <74008142+Vladox69@users.noreply.github.com>
2021-01-15 23:21:20 -0500

    Activity listas, crear usuarios con cards y recyclers

---
+++ app/src/main/java/com/example/pruebas/listasUsuario.java
@@ -0,0 +1,65 @@
+package com.example.pruebas;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.example.pruebas.entidades.ComprasPlanificadas;
+import com.example.pruebas.entidades.Producto;
+
+import java.util.ArrayList;
+
+public class listasUsuario extends AppCompatActivity {
+    ConexionSQLiteHelper conn;
+    ArrayList<ComprasPlanificadas> listaCompras;
+    RecyclerView recyclerListas;
+    SharedPreferences preferences;
+    String cedula;
+    String nombre;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listas_usuario);
+
+        conn=new ConexionSQLiteHelper(getApplicationContext());
+
+        recyclerListas= (RecyclerView) findViewById(R.id.rvListasUsuario);
+        recyclerListas.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL,false));
+
+        preferences=getSharedPreferences("Preferences",MODE_PRIVATE);
+        cedula=preferences.getString("CED_USU",null);
+        nombre=preferences.getString("NOM_USU",null);
+
+        cargarListaCompras();
+
+        if(listaCompras.size()!=0){
+            adapterListas adapter=new adapterListas(listaCompras);
+            recyclerListas.setAdapter(adapter);
+        }
+
+    }
+
+    public void cargarListaCompras(){
+        SQLiteDatabase db=conn.getReadableDatabase();
+        ComprasPlanificadas compra=null;
+        listaCompras=new ArrayList<ComprasPlanificadas>();
+        Cursor cursor =db.rawQuery("SELECT * FROM COMPRAS_PLANIFICADAS WHERE CED_USU_COM='"+cedula+"'",null);
+        while (cursor.moveToNext()){
+            compra=new ComprasPlanificadas();
+            compra.setNumeroCompra(cursor.getString(0));
+            compra.setNombreCompra(cursor.getString(1));
+            compra.setCedulaUsuario(cursor.getString(2));
+            Log.i("Cate",compra.getNombreCompra());
+            listaCompras.add(compra);
+        }
+        conn.close();
+    }
+
+}
