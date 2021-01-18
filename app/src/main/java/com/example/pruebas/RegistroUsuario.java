011f980b24684a9834d993596a07036b1102368d
Paulo1603C <74008996+Paulo1603C@users.noreply.github.com>
2021-01-18 12:14:19 -0500

    Agragado los metodos en la activity_registro_usuario, para cuando el usuario ya exista no le deje registrarse

--- app/src/main/java/com/example/pruebas/RegistroUsuario.java
+++ app/src/main/java/com/example/pruebas/RegistroUsuario.java
@@ -3,6 +3,8 @@ package com.example.pruebas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
@@ -21,8 +23,17 @@ public class RegistroUsuario extends AppCompatActivity {
         nombre=(EditText)findViewById(R.id.txtNombre);
     }
 
-    public void llamadoMetodoRegistro(View view){
-        registrarUsuarios();
+
+    public boolean userExit(){
+        conn =new ConexionSQLiteHelper(getApplicationContext());
+        SQLiteDatabase baseDatos = conn.getWritableDatabase();
+        String ced = cedula.getText().toString();
+        Cursor fila = baseDatos.rawQuery("select * from USUARIOS where CED_USU =" + ced, null);
+        if ( fila.moveToFirst() ){
+            return true;
+        }else{
+            return false;
+        }
     }
 
     public void registrarUsuarios(){
@@ -39,7 +50,43 @@ public class RegistroUsuario extends AppCompatActivity {
         Toast.makeText(getApplicationContext(),"Usuario: "+idResultante,Toast.LENGTH_SHORT).show();
 
         conn.close();
-
     }
+
+    public void guardarUsuarios(View view){
+        String ced = cedula.getText().toString();
+        String nom = nombre.getText().toString();
+        if( !ced.isEmpty() || !nom.isEmpty()  ){
+            //usuario.add(cedula.getText().toString());
+            if( userExit() == true ){
+                Toast.makeText(this, "EL usuario ya existe", Toast.LENGTH_SHORT).show();
+                conn.close();
+            }else {
+                registrarUsuarios();
+                Intent categorias = new Intent(this, MainActivity.class);
+                startActivity(categorias);
+            }
+        }else{
+            Toast.makeText(this, "Llene los campos", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    //Method insert opcional
+    /*public void registrar(){
+        conn=new ConexionSQLiteHelper(getApplicationContext());
+        SQLiteDatabase baseDatos = conn.getWritableDatabase();
+        String ced = cedula.getText().toString();
+        String nom = nombre.getText().toString();
+        ContentValues registro = new ContentValues();
+        registro.put("CEDULA", ced);
+        registro.put("NOMBRE", nom);
+
+        baseDatos.insert("USUARIOS", null, registro);
+        baseDatos.close();
+        cedula.setText("");
+        nombre.setText("");
+        //mensage de registro existo;
+        Toast.makeText(this, "registro existo", Toast.LENGTH_SHORT).show();
+    }*/
+
 
 }
